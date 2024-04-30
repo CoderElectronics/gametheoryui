12563d8ee2c030ec2dba9567c449346ec0ae1d5f
CoderElectronics <aristehney@gmail.com>
2024-04-30 15:39:17 -0400

    add newest stuff

--- imported_strategy_test.py
+++ imported_strategy_test.py
@@ -9,7 +9,11 @@ class ImportedStrat(GameStrategy):
     def __init__(self):
         super().__init__(name="Imported Strategy", author="Ari S.", description="my imported script file")
 
-    def next_play(self):
+    # history of your moves and if you won
+    def next_play(history):
+        return # {
+
+        # }[[0, 1, 2],[0,1,2]]
         pass
 
 # Do not change, userGame is a preset variable that will be imported.
